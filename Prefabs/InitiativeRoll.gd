cc582dc49b1a5566066b6046bf0c5f99c685105d
Gabriel Ikezaki <gabriel.ikezaki@gmail.com>
2025-04-02 16:34:46 -0700

    Refactor: player health labels and initiative name labels are dynamically added into the world scene in respect to the number of players

--- Prefabs/InitiativeRoll.gd
+++ Prefabs/InitiativeRoll.gd
@@ -9,11 +9,17 @@ extends CanvasLayer
 signal end_initiative
 
 export (NodePath) var ready_path
+export (NodePath) var vbox_path
+
+export (Resource) var player_label
 
 var started = false # Prevents player from playing fadeout animation when they shouldn't
 var players = [] # List of the name of the players to be displayed in the initiative order
+var labels = []
 
 onready var ready = get_node(ready_path)
+onready var vbox = get_node(vbox_path)
+
 func _input(event):
 	if !started:
 		return
@@ -21,11 +27,26 @@ func _input(event):
 		play_fade_out()
 
 ###
+# Called when this object receives the broadcasted message that the intro has ended.
+# Sets writes the players' names to the labels. 
+# @param players a list of strings containing the names of all players
+###
+func set_initiative_labels(players):
+	for i in range(len(players)):
+		var new_label = player_label.instance()
+		new_label.text = players[i]
+		vbox.add_child(new_label)
+		labels.append(new_label)
+		pass
+
+###
 # This is called when this object receives the broadcasted message that the intro has ended.
 # It starts playing the FadeIn animation.
 ###
 func play_fade_in():
 	get_node("AnimationPlayer").play("FadeIn_Initiative")
+	for l in labels:
+		l.get_node("AnimationPlayer").play("fade_in")
 
 ###
 # Called when the player presses a confirm key after the initiative order is fully displayed.
@@ -33,6 +54,8 @@ func play_fade_in():
 ###
 func play_fade_out():
 	get_node("AnimationPlayer").play("FadeOut_Initiative")
+	for l in labels:
+		l.get_node("AnimationPlayer").play("fade_out")
 
 ###
 # Called by AnimationPlayer when the FadeIn animation finishes. Lets player press the confirm
@@ -55,12 +78,3 @@ func end_initiative_roll():
 func close():
 	self.visible = false
 	started = false
-
-###
-# Called when this object receives the broadcasted message that the intro has ended.
-# Sets writes the players' names to the labels. 
-# @param players a list of strings containing the names of all players
-###
-func set_initiative_labels(players):
-	get_node("MarginContainer/VBoxContainer/First").text = players[0]
-	get_node("MarginContainer/VBoxContainer/Second").text = players[1]
--- Prefabs/UI.gd
+++ Prefabs/UI.gd
@@ -1,13 +1,19 @@
 extends CanvasLayer
 
+export (Resource) var player_health
+export (NodePath) var hbox_path
+
 var p1_failure
 var p2_failure
 var round_count
 var player_turn
 
 var characters = []
-var labels = []
+var health_labels = []
 var gamestatemanager
+
+onready var hbox = get_node(hbox_path)
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	gamestatemanager = get_node("/root/Board/GameManager/GameStateManager")
@@ -18,12 +24,11 @@ func _ready():
 	round_count = get_node("RoundCount")
 	player_turn = get_node("PlayerTurn")
 	
-	for c in get_node("MarginContainer/HBoxContainer").get_children():
-		labels.append(c)
 	for i in get_node("/root/Board/GameManager/GameStateManager").get_initiative_order():
-		characters.append(i.get_node("HealthManager"))
+		characters.append(i)
 	for i in range(len(characters)):
-		characters[i].connect("health_change",self,"update_health",[i])
+		var health_manager = characters[i].get_node("HealthManager")
+		health_manager.connect("health_change",self,"update_health",[i])
 	pass
 
 func get_character_hp(path):
@@ -39,12 +44,16 @@ func update_player_turn():
 	pass
 
 func update_health(idx):
-	labels[idx].text = String(characters[idx].get_curr_health())
+	var health_manager = characters[idx].get_node("HealthManager")
+	health_labels[idx].text = "Player " + String(characters[idx].get_id()) + ": " + String(health_manager.get_curr_health())
 
 func on_game_end():
 	 get_node("End").visible = true
 
 func on_ready_ended():
 	for i in range(len(characters)):
+		var new_health_label = player_health.instance()
+		hbox.add_child(new_health_label)
+		health_labels.append(new_health_label)
 		update_health(i)
 	pass
